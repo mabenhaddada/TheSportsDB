47fee7782d8ba99a6f456246c1bc63985e123f44
Amine BEN HADDADA <ben.haddada.mohamed.amine@gmail.com>
2025-06-05 23:25:40 +0200

    Initial Commit

---
+++ TheSportsDB/TheSportsDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSportsDBApp.swift
+//  TheSportsDB
+//
+//  Created by Mohamed Amine BEN HADDADA on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TheSportsDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
